5198baaa57f0b7c94071bc30fb6e9a3ce9cdeea1
Suzuki Masato <masato05rock27@gmail.com>
2022-10-24 09:52:22 +0900

    initial commit

---
+++ src/main/java/com/example/web/hello/HelloPage.java
@@ -0,0 +1,23 @@
+package com.example.web.hello;
+
+public class HelloPage {
+
+	private String name = "Seaser2 from Maven !!";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Class initialize() {
+		return null;
+	}
+
+	public Class prerender() {
+		return null;
+	}
+
+}
