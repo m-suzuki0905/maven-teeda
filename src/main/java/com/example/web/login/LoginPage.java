2fe753743dca0ff6ad9e5d6f35b2e0869702c3dd
Suzuki Masato <masato05rock27@gmail.com>
2022-10-26 13:28:36 +0900

    ログインページの仮作成

--- src/main/java/com/example/web/login/LoginPage.java
+++ src/main/java/com/example/web/login/LoginPage.java
@@ -1,27 +1,61 @@
 package com.example.web.login;
 
+import org.seasar.teeda.extension.annotation.validator.Email;
+import org.seasar.teeda.extension.annotation.validator.Length;
+import org.seasar.teeda.extension.annotation.validator.Required;
+
+import com.example.web.member.UserPage;
+
 public class LoginPage {
-  private String title = "Login";
+  private String titleValue = "LoginPage";
   private String sign;
 
-  public String getTitle() {
-    return title;
+  private String email;
+
+  private String password;
+
+  public String getTitleValue() {
+    return titleValue;
   }
 
   public String getSign() {
     return sign;
   }
 
+  public String getEmail() {
+    return email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  @Required(target = "doUserPage", messageId = "login.form.email.value")
+  @Email(messageId = "login.form.email")
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  @Required(target = "doUserPage", messageId = "login.form.password.value")
+  @Length(minimum = 4, minimumMessageId = "login.form.password")
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
   public Class<?> getLayout() {
     return null;
   }
 
-  public void setTitle(String title) {
-    this.title = title;
+  public void setTitleValue(String titleValue) {
+    this.titleValue = titleValue;
   }
 
   public void setSign(String sign) {
     this.sign = sign;
+  }
+
+  public Class<?> doUserPage() {
+    return UserPage.class;
   }
 
   public Class initialize() {
