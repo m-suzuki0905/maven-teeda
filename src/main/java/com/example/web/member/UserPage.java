04130f5f340aeb4bc71fc767c0f037a2f1f7f010
Suzuki Masato <masato05rock27@gmail.com>
2022-10-26 13:29:24 +0900

    Topメニューのひな型のみ作成

---
+++ src/main/java/com/example/web/member/UserPage.java
@@ -0,0 +1,14 @@
+package com.example.web.member;
+
+public class UserPage {
+  private String message;
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+}
